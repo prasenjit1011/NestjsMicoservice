8b19edf6c1507f23095280c38f6070418eb1dbe6
Prasenjit <prasenjit.aluni@gmail.com>
2025-03-02 17:53:37 +0530

    Nestjs with RabbitMQ

--- apps/student/src/main.ts
+++ apps/student/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+
 import { StudentModule } from './student.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(StudentModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://guest:guest@rabbitmq:5672'], // RabbitMQ connection URL
+      queue: 'students_queue', // Name of the queue
+      queueOptions: { durable: false },
+    },
+  });
+  await app.startAllMicroservices();
+
   await app.listen(3001);
 }
 bootstrap();
--- apps/student/src/student.service.ts
+++ apps/student/src/student.service.ts
@@ -1,11 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { Ctx, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
 
 @Injectable()
 export class StudentService {
+
+  @MessagePattern('get_students') // Listening for messages on 'get_students' queue
+  handleGetStudents(@Payload() data: any, @Ctx() context: RmqContext) {
+    console.log('Received:', data);
+
+    // Acknowledge message to prevent re-processing
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    channel.ack(originalMsg);
+
+    return [{ id: 1, name: 'John Doe' }, { id: 2, name: 'Jane Doe' }];
+  }
+
+
+
+
   getHello() {
-    let str = 'Hello World ! Student';
+    let str = 'Hello World ! Student Service';
     console.log(str);
 
     return str;
   }
 }
+
+
+
